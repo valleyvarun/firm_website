7a1ac89adbccf712f519b4752a3fe561bdca96bb
Varun A. Sridhar <Valleyvarun@gmail.com>
2025-04-21 19:15:42 +0530

    add website files please

---
+++ script.js
@@ -0,0 +1,47 @@
+// Fetch the content of the .txt file and display it in the #about-text div
+fetch('about_us.txt') // Replace 'about.txt' with the path to your .txt file
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch the text file.');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('about-text').innerHTML = `<p>${data}</p>`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('about-text').innerHTML = `<p>Failed to load content.</p>`;
+    });
+
+    // Fetch the content of the .txt file and display it in the #about-text div
+fetch('about_us2.txt') // Replace 'about.txt' with the path to your .txt file
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Failed to fetch the text file.');
+    }
+    return response.text();
+})
+.then(data => {
+    document.getElementById('about-text2').innerHTML = `<p>${data}</p>`;
+})
+.catch(error => {
+    console.error('Error:', error);
+    document.getElementById('about-text2').innerHTML = `<p>Failed to load content.</p>`;
+});
+
+// Fetch the content of the .txt file and display it in the #about-text div
+fetch('about_us3.txt') // Replace 'about.txt' with the path to your .txt file
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch the text file.');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('about-text3').innerHTML = `<p>${data}</p>`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('about-text3').innerHTML = `<p>Failed to load content.</p>`;
+    });
